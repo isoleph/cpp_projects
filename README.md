2e8b447eb7fd4ab389b9659aca16cc09c863a1a0
Angel V <valdenegro.angel@gmail.com>
2019-11-02 16:15:46 -0700

    Organizing code into files

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
     <img src=img/cpp.png width = 150>
 </div>
 
-This is a repository full of random projects or ideas that I work on in C++. Here's a short description of the files you can find in here.
+This is a repository full of random projects or ideas that I work on in C++. Here's a short description of the files you can find in the folders above.
 
 **FourVector.cpp**: A custom class to do operations on the [four-vector](https://en.wikipedia.org/wiki/Four-vector) mathematical object in Einstein's Relativity. Includes boosts, dual vectors, and other vector operations. 
 
@@ -21,7 +21,7 @@ This is a repository full of random projects or ideas that I work on in C++. Her
 </div>
 
 
-**RK4.cpp** : A script that uses the [Runge-Kutta Method](https://en.wikipedia.org/wiki/Runge–Kutta_methods) to solve ordinary differential equations. I also wrote `rk4_analysis.py` to compare the RK4 solution to the actual solution.
+**RK4.cpp** : A script that uses the [Runge-Kutta Method](https://en.wikipedia.org/wiki/Runge–Kutta_methods) to solve ordinary differential equations. I also wrote `rk4_analysis.py` to compare the RK4 solution to the actual solution. In this folder, I've also included RK42D which solves second order ODES the same way, as well as a function to calculate derivatives at a point with `derivatives.cpp`.
 
 
 <div align="center">
---
+++ RK4/RK42D.cpp
@@ -0,0 +1,131 @@
+// g++ -std=c++17 -o RK42D RK42D.cpp
+/****************************************************
+* Name: RK4.cpp									    *
+* Description: A script to solve 2-order ODEs       * 
+*   using the Runge-Kutta Method                    *
+* Supports:                                         *
+*   - Single var integration                        *
+* Author: Angel A. Valdenegro                       *
+* TODO: More tests with different functions         *
+*****************************************************/ 
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <functional>
+
+
+
+// create shorthand for types later 
+#define pi 3.14159265
+typedef std::function<double (double, double, double)> function;
+typedef std::vector<double> vector;
+
+
+// test function
+double myFunction(double x, double y, double yd) {
+    return sin(x);
+}
+
+// R-K Class where the magic happens
+class RK4 {
+    private: 
+        double k1n, k2n, k3n, k4n;          // First-Order Runge-Kutta Numbers
+        double c1n, c2n, c3n, c4n;          // Define Second-Order Runge-Kutta Numbers
+
+
+        //** Functions to calculate Second-Order RK Numbers **//
+        double c1 (double ydn, double h) {                                              // c1n
+            c1n = ydn;
+            return c1n;
+        }
+        double c2 (double ydn, double h) {                                              // c2n
+            c2n = ydn + 0.5 * h * k1n;
+            return c2n;
+        }
+        double c3 (double ydn, double h ) {                                             // c3n
+            c3n = ydn + 0.5 * h * k2n;
+            return c3n;
+        }
+        double c4 (double ydn, double h) {                                              // c4n
+            c4n = ydn + h * k3n;
+            return c4n;
+        }
+
+
+        //** Functions to calculate First-Order RK Numbers**//
+        double k1 (double t, double y, double ydn, double h, function func) {           // k1n
+            k1n = func(t, y, ydn);
+            return k1n;
+        }
+        double k2 (double t, double y, double ydn, double h, function func) {           // k2n
+            k2n = func(t + h/2, y + h/2 * c1(ydn, h), ydn);
+            return k2n;
+        }
+        double k3 (double t, double y, double ydn, double h, function func) {           // k3n
+            k3n = func(t + h/2, y + h/2 * c2(ydn, h), ydn);
+            return k3n;
+        }
+        double k4 (double t, double y, double ydn, double h, function func) {           // k4n
+            k4n = func(t + h, y + h * c3(ydn, h), ydn);
+            return k4n;
+        }
+
+    public:
+        // Store values in an array if needed later 
+        vector xvalues;
+        vector yvalues;
+
+        // initial t-value t_0, final t-value t_f, initial y-value y_0, numbers of steps n
+        vector iterator(double t_0, double t_f, double y_0, double yd_0, int n = 100, function func = myFunction) {
+
+            double h = (t_f - t_0)/n;           // calculate incremental distances
+            double y_n = y_0;                   // initial y value
+            double yd_n = yd_0;                 // initial y' value
+
+            vector xpoints;                     // keep track of x-coordinates
+            vector ypoints;                     // array to save solutions
+
+            for(int i = 0; i <= n; i++) {
+                double increment = h * i;
+                xpoints.push_back(t_0 + increment);
+                ypoints.push_back(y_n);
+
+                yd_n += 0.166667 * h * ( 
+                        k1(t_0 + increment, y_n, yd_n, h, func)
+                  + 2 * k2(t_0 + increment, y_n, yd_n, h, func) 
+                  + 2 * k3(t_0 + increment, y_n, yd_n, h, func) 
+                      + k4(t_0 + increment, y_n, yd_n, h, func) 
+                );
+
+                y_n += h * (yd_n + h * 0.166667 * (
+                        c1(yd_n, h)
+                  + 2 * c2(yd_n, h)
+                  + 2 * c3(yd_n, h)
+                      + c4(yd_n, h)
+                    )
+                );
+            }
+
+            xvalues = xpoints;
+            yvalues = ypoints;
+            return yvalues;
+        }
+};
+
+
+int main() {
+    RK4 object;
+    vector results = object.iterator(0, 2*pi, 0, -1, 100, myFunction);
+
+    std::ofstream outfile;
+    outfile.open("RK42O.csv");
+    for (int i = 0; i < results.size(); i++) {
+        outfile << object.xvalues[i] << ", " <<  object.yvalues[i] << std::endl;
+    }
+    outfile.close();
+    return 0;
+}
+
+
